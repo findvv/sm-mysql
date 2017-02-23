c136fc963173c1f04beb20c4e608bc69f433745f
findvv <zzxzzx2015@126.com>
2017-02-23 18:02:20 +0800

    移除

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 'use strict';
 module.exports = {  
   host     : '58ad3dcc593b0.gz.cdb.myqcloud.com',  
-  user     : 'cdb_outerroot',  
-  password : 'ZZX137623',  
+  user     : '',  
+  password : '',  
   database : 'test1',
   port     : '3724',
   table    : 'test1'
