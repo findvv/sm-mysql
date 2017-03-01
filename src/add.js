6f4682f224e5d50ad636c37f9adedda1dcd7e93a
findvv <zzxzzx2015@126.com>
2017-03-01 15:31:43 +0800

    插入多条数据

--- src/add.js
+++ src/add.js
@@ -19,7 +19,7 @@ module.exports = {
         if (!obj.length) {
             for(var k in obj) {
                 arr1.push(k);
-                arr2.push('"' + obj[k] + '"');
+                arr2.push(obj[k]);
             }
             str1 = '(' + String(arr1) + ')';
             str2 = '(' + String(arr2) + ')';
@@ -30,14 +30,13 @@ module.exports = {
             str1 = '(' + String(arr1) + ')';
             for(var v of obj) {
                 arr3 = [];
-                for(var i in obj[v]) {
-                    arr3.push('"' + obj[v][i] + '"');
+                for(var i in v) {
+                    arr3.push(v[i]);
                 }
-                arr2.push(String(arr3));
+                arr2.push('(' + String(arr3) + ')');
             }
             str2 = String(arr2);
         }
-        
         connection.query(`INSERT INTO ${table} ${str1} VALUES ${str2}`, function(err, rows, fields) {
             that.startNum += 1;
             err ? that.result.push(err) : that.result.push(rows);
