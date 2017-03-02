9b97c53e772a708c327285391a9ddd9f1139b974
findvv <zzxzzx2015@126.com>
2017-03-02 18:08:30 +0800

    修改测试用例

--- test/test_copyTable.js
+++ test/test_copyTable.js
@@ -1,4 +1,5 @@
-var SMysql = require('../index.js')
+var SMysql = require('../index.js');
+var extend = require('extend');
 var db1 = require('../dbs/db1.js');
 var db2 = require('../dbs/db2.js');
-SMysql.copyTable('aqi', db1, db2);
+SMysql.copyTable('aqi', extend(db1,{database:'weather'}), extend(db2,{database:'weather'}));
